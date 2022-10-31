ffe62eb1813adacd7451851c6e98abea918ef0cc
RIccardoIzzo <izzo.riccardo38@gmail.com>
2022-10-31 08:55:26 +0100

    update KNNCBF

--- models/ItemKNNCBF_test.py
+++ models/ItemKNNCBF_test.py
@@ -1,5 +1,9 @@
 import pandas as pd
+import numpy as np
+
 from Utils.DataLoaderSplit import DataLoaderSplit
+from Evaluation.Evaluator import EvaluatorHoldout
+from Recommenders.Similarity.Compute_Similarity_Python import *
 from Utils.evaluation_n_metrics import evaluate
 from Utils.prep_sub import write_csv
 from Utils.preproc_n_split import preproc_n_split
@@ -7,79 +11,135 @@ from Utils.preproc_n_split import preproc_n_split
 # Hyperparameter
 test_split = 0.2
 validation_split = 0.1
-shrink = 350000000
+shrink = 100000
 slice_size = 100
 
 # URM_all_dataframe, ICM_dataframe2, ICM_dataframe = read_data()
 data_loader = DataLoaderSplit()
-train, test, val, n_episode_list, ICM_dataframe = data_loader.get_csr_matrices()
-"""
-URM_all_dataframe, num_users, num_items, urm_train, urm_validation, urm_test = preproc_n_split(URM_all_dataframe, test_split=test_split,
-                                                                                 val_split=validation_split)
-
-mapped_id, original_id = pd.factorize(URM_all_dataframe["user_id"].unique())
-
-print("Unique UserID in the URM are {}".format(len(original_id)))
-
-user_original_ID_to_index = pd.Series(mapped_id, index=original_id)
-
-mapped_id, original_id = pd.factorize(URM_all_dataframe["item_id"].unique())
-
-print("Unique ItemID in the URM are {}".format(len(original_id)))
-
-all_item_indices = pd.concat([URM_all_dataframe["item_id"], ICM_dataframe["item_id"]], ignore_index=True)
-mapped_id, original_id = pd.factorize(all_item_indices.unique())
-
-print("Unique ItemID in the URM and ICM are {}".format(len(original_id)))
-
-item_original_ID_to_index = pd.Series(mapped_id, index=original_id)
-
-mapped_id, original_id = pd.factorize(ICM_dataframe["feature_id"].unique())
-feature_original_ID_to_index = pd.Series(mapped_id, index=original_id)
-
-print("Unique FeatureID in the URM are {}".format(len(feature_original_ID_to_index)))
-
-URM_all_dataframe["user_id"] = URM_all_dataframe["user_id"].map(user_original_ID_to_index)
-URM_all_dataframe["item_id"] = URM_all_dataframe["item_id"].map(item_original_ID_to_index)
-"""
+iai = data_loader.get_iai()
+train, test, val, n_episode_list, ICM_dataframe = data_loader.get_all_csr_matrices()
 
 
-
-
-# import numpy as np
-# import scipy.sparse as sp
-# from similarity_functions.cosine_similarity_SLOW import cosine_similarity
-# from typing import Optional
-
-
-
-
-import scipy.sparse as sps
-
-"""
-
-ICM_genres = genre_list["feature_id"]
-stacked_URM = sps.vstack([urm_train, ICM_genres.T])
-stacked_URM = sps.csr_matrix(stacked_URM)
-stacked_ICM = sps.csr_matrix(stacked_URM.T)
-"""
-
-# n_users = len(user_original_ID_to_index)
-# n_items = len(item_original_ID_to_index)
-# n_features = len(feature_original_ID_to_index)
-# ICM_all = sps.csr_matrix(
-#     (np.ones(len(ICM_dataframe["item_id"].values)), (ICM_dataframe["item_id"].values, ICM_dataframe["feature_id"].values)),
-#     shape=(n_items, n_features))
-#
-# ICM_all.data = np.ones_like(ICM_all.data)
+evaluator_validation = EvaluatorHoldout(val, cutoff_list=[10])
+evaluator_test = EvaluatorHoldout(train, cutoff_list=[10])
 
 # ##############################################	recommender part	 ############################################# #
 
+### recommender without evaluation, fix write_csv
+
+# class ItemKNNCBFRecommender(object):
+#
+#     def __init__(self, URM, ICM):
+#         self.URM = URM
+#         self.ICM = ICM
+#
+#     def fit(self, topK=50, shrink=100, normalize=True, similarity="cosine"):
+#         similarity_object = Compute_Similarity_Python(self.ICM.T, shrink=shrink,
+#                                                       topK=topK, normalize=normalize,
+#                                                       similarity=similarity)
+#
+#         self.W_sparse = similarity_object.compute_similarity()
+#
+#     def recommend(self, user_id, at=None, exclude_seen=True):
+#         # compute the scores using the dot product
+#         user_profile = self.URM[user_id]
+#         scores = user_profile.dot(self.W_sparse).toarray().ravel()
+#
+#         if exclude_seen:
+#             scores = self.filter_seen(user_id, scores)
+#
+#         # rank items
+#         ranking = scores.argsort()[::-1]
+#
+#         return ranking[:at]
+#
+#     def filter_seen(self, user_id, scores):
+#         start_pos = self.URM.indptr[user_id]
+#         end_pos = self.URM.indptr[user_id + 1]
+#
+#         user_profile = self.URM.indices[start_pos:end_pos]
+#
+#         scores[user_profile] = -np.inf
+#
+#         return scores
+#
+#
+# recommender = ItemKNNCBFRecommender(train, ICM_dataframe)
+# recommender.fit(shrink=100000, topK=50)
+#
+# for user_id in range(100):
+#     print(recommender.recommend(user_id, at=10))
+#
+# write_csv(iai, train, val, recommender)
+
+
+
+
+
 from Recommenders.KNN.ItemKNNCBFRecommender import ItemKNNCBFRecommender
 
-recommender_ItemKNNCBF = ItemKNNCBFRecommender(URM_all_dataframe, ICM_dataframe)
+x_tick_grid = [10, 50, 100, 150, 200, 250, 300, 350, 400, 450, 500]
+MAP_per_k_grid = []
 
-recommender_ItemKNNCBF.fit()
+# for topK in x_tick_grid:
+recommender = ItemKNNCBFRecommender(train, ICM_dataframe)
+recommender.fit(shrink=100000, topK=50)
+
+result_df, _ = evaluator_test.evaluateRecommender(recommender)
+
+MAP_per_k_grid.append(result_df.loc[10]["MAP"])
+print(MAP_per_k_grid[0])
+
+
+
+#
+# for user_id in range(10):
+#     print(recommender.recommend(user_id, at=10))
+#
+# write_csv(iai, train, val, recommender)
+
+
+# from Recommenders.KNN.ItemKNNCBFRecommender import ItemKNNCBFRecommender
+# recommender = ItemKNNCBFRecommender(train, ICM_dataframe)
+# recommender.fit(shrink=1000, topK=50)
+# for user_id in range(10):
+#     print(recommender.recommend(user_id, at=10))
+#
+# MAP_per_k_grid = []
+# result_df, _ = evaluator_test.evaluateRecommender(recommender)
+#
+# MAP_per_k_grid.append(result_df.loc[10]["MAP"])
+# print(MAP_per_k_grid[0])
+
+# from Recommenders.KNN.ItemKNNCBFRecommender import ItemKNNCBFRecommender
+#
+#
+# MAP_per_shrinkage_grid = []
+# recommender = ItemKNNCBFRecommender(train, ICM_dataframe)
+# recommender.fit(shrink=10, topK=100)
+#
+# result_df, _ = evaluator_test.evaluateRecommender(recommender)
+#
+# MAP_per_shrinkage_grid.append(result_df.loc[10]["MAP"])
+# print(result_df.loc[10])
+
+# accum_precision, accum_recall, accum_map, num_user_evaluated, num_users_skipped = evaluate(recommender_ItemKNNCBF,
+#                                                                                            train,
+#                                                                                            test)
+
+# x_tick_grid = [0, 10, 50, 100, 200, 300, 400, 500]
+# MAP_per_shrinkage_grid = []
+#
+# for shrink in x_tick_grid:
+#     recommender = ItemKNNCBFRecommender(train, ICM_dataframe)
+#     recommender.fit(shrink=shrink, topK=100)
+#
+#     result_df, _ = evaluator_test.evaluateRecommender(recommender)
+#
+#     MAP_per_shrinkage_grid.append(result_df.loc[10]["MAP"])
+#
+# for i in range(0, len(MAP_per_shrinkage_grid)):
+#     print(MAP_per_shrinkage_grid[i])
 
 ########################################################################################################################
 
@@ -87,4 +147,4 @@ recommender_ItemKNNCBF.fit()
 #                                                                                            urm_train,
 #                                                                                            urm_test)
 
-#write_csv(URM_all_dataframe, urm_train, urm_validation, recommender_ItemKNNCBF)
+# write_csv(URM_all_dataframe, urm_train, urm_validation, recommender_ItemKNNCBF)
