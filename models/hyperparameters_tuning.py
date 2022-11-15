d7ea5f96acf2d10c52c8cc4277200dce288ae65f
RiccardoIzzo <riccardo.izzo@mail.polimi.it>
2022-11-15 23:53:01 +0100

    minor update

--- models/hyperparameters_tuning.py
+++ models/hyperparameters_tuning.py
@@ -7,6 +7,8 @@ from Recommenders.DataIO import DataIO
 from Data_manager.split_functions.split_train_validation_random_holdout import \
     split_train_in_two_percentage_global_sample
 from Recommenders.DataIO import DataIO
+from skopt.space import Real, Integer, Categorical
+# from HyperparameterTuning.SearchAbstractClass import SearchInputRecommenderArgs
 
 from Recommenders.KNN.ItemKNNCFRecommender import ItemKNNCFRecommender
 from Recommenders.EASE_R.EASE_R_Recommender import EASE_R_Recommender
@@ -32,20 +34,20 @@ URM_train, URM_validation = split_train_in_two_percentage_global_sample(URM_trai
 evaluator_validation = EvaluatorHoldout(URM_validation, cutoff_list=[10])
 evaluator_test = EvaluatorHoldout(URM_test, cutoff_list=[10])
 
-import scipy.sparse as sp
-icm_mixed = sp.hstack([ICM_length, ICM_type])
+# import scipy.sparse as sp
+# icm_mixed = sp.hstack([ICM_length, ICM_type])
 # urm_icm_stacked = sp.vstack([URM_train, icm_mixed.T])
-urm_stacked_mix_train = sp.vstack([URM_train, icm_mixed.T])
-urm_stacked_mix_train_validation = sp.vstack([URM_train_validation, icm_mixed.T])
+# urm_stacked_mix_train = sp.vstack([URM_train, icm_mixed.T])
+# urm_stacked_mix_train_validation = sp.vstack([URM_train_validation, icm_mixed.T])
 
 ### CHOOSE RECOMMENDER HERE ###
-recommender_class = ItemKNNCFRecommender
+recommender_class = LightFMCFRecommender
 
 # hyperparameterSearch = SearchBayesianSkopt(recommender_class,
-#                                                evaluator_validation=evaluator_validation,
-#                                                evaluator_test=evaluator_test)
+#                                            evaluator_validation=evaluator_validation,
+#                                            evaluator_test=evaluator_test)
 
-n_cases = 10
+n_cases = 50
 n_random_starts = int(n_cases * 0.3)
 metric_to_optimize = "MAP"
 cutoff_to_optimize = 10
@@ -53,15 +55,45 @@ cutoff_to_optimize = 10
 # similarity_type_list=['cosine', 'jaccard', "asymmetric", "dice", "tversky", "tanimoto"],
 
 # hyperparameters_range_dictionary = {
-#     "topK": Integer(900, 1500),
-#     "l1_ratio": Real(low=1e-5, high=1e-2, prior='log-uniform'),
-#     "alpha": Real(low=1e-2, high=1.0, prior='uniform'),
-#     "ICM_weight": Real(low=1e-1, high=1e4, prior='log-uniform'),
+#     "topK": Integer(1000, 10000),
+#     "shrink": Integer(200, 10000),
+#     "similarity": Categorical(['cosine']),
+#     "normalize": Categorical([True]),
+#     "feature_weighting": Categorical(['TF-IDF'])
 # }
+#
+# recommender_input_args = SearchInputRecommenderArgs(
+#     CONSTRUCTOR_POSITIONAL_ARGS=[URM_train],  # For a CBF model simply put [URM_train, ICM_train]
+#     CONSTRUCTOR_KEYWORD_ARGS={},
+#     FIT_POSITIONAL_ARGS=[],
+#     FIT_KEYWORD_ARGS={},
+#     EARLYSTOPPING_KEYWORD_ARGS={},
+# )
+#
+# recommender_input_args_last_test = SearchInputRecommenderArgs(
+#     CONSTRUCTOR_POSITIONAL_ARGS=[URM_train_validation],  # For a CBF model simply put [URM_train_validation, ICM_train]
+#     CONSTRUCTOR_KEYWORD_ARGS={},
+#     FIT_POSITIONAL_ARGS=[],
+#     FIT_KEYWORD_ARGS={},
+#     EARLYSTOPPING_KEYWORD_ARGS={},
+# )
+#
+# hyperparameterSearch.search(recommender_input_args,
+#                             recommender_input_args_last_test=recommender_input_args_last_test,
+#                             hyperparameter_search_space=hyperparameters_range_dictionary,
+#                             n_cases=n_cases,
+#                             n_random_starts=n_random_starts,
+#                             save_model="best",
+#                             output_folder_path='../trained_models/',  # Where to save the results
+#                             output_file_name_root=recommender_class.RECOMMENDER_NAME,  # How to call the files
+#                             metric_to_optimize=metric_to_optimize,
+#                             cutoff_to_optimize=cutoff_to_optimize,
+#                             resume_from_saved=True,
+#                             )
 
 runHyperparameterSearch_Collaborative(recommender_class,
-                                      URM_train=urm_stacked_mix_train,
-                                      URM_train_last_test=urm_stacked_mix_train_validation,
+                                      URM_train=URM_train,
+                                      URM_train_last_test=URM_train_validation,
                                       metric_to_optimize=metric_to_optimize,
                                       cutoff_to_optimize=cutoff_to_optimize,
                                       evaluator_validation_earlystopping=evaluator_validation,
@@ -75,18 +107,6 @@ runHyperparameterSearch_Collaborative(recommender_class,
                                       similarity_type_list=['cosine'],
                                       n_cases=n_cases,
                                       n_random_starts=n_random_starts)
-
-# hyperparameterSearch.search(recommender_input_args,
-#                             recommender_input_args_last_test=recommender_input_args_last_test,
-#                             hyperparameter_search_space=hyperparameters_range_dictionary,
-#                             n_cases=n_cases,
-#                             n_random_starts=n_random_starts,
-#                             save_model="best",
-#                             output_folder_path='../trained_models/',  # Where to save the results
-#                             output_file_name_root=recommender_class.RECOMMENDER_NAME,  # How to call the files
-#                             metric_to_optimize=metric_to_optimize,
-#                             cutoff_to_optimize=cutoff_to_optimize,
-#                             )
 
 # CBF and so on...
 # runHyperparameterSearch_Content(recommender_class,
@@ -130,20 +150,20 @@ runHyperparameterSearch_Collaborative(recommender_class,
 
 ################### VISUALIZE RESULTS FOR HYPERPARAMETER SEARCH ########################
 
-data_loader = DataIO(folder_path='../trained_models')
-search_metadata = data_loader.load_data(recommender_class.RECOMMENDER_NAME + "_metadata.zip")
-search_metadata.keys()
-
-hyperparameters_df = search_metadata["hyperparameters_df"]
-print(hyperparameters_df)
-
-result_on_validation_df = search_metadata["result_on_validation_df"]
-print(result_on_validation_df)
-
-result_best_on_test = search_metadata["result_on_last"]
-print(result_best_on_test)
-
-best_hyperparameters = search_metadata["hyperparameters_best"]
-print(best_hyperparameters)
-
-print("FINAL MAP: {}".format(result_best_on_test.loc[10]["MAP"]))
+# data_loader = DataIO(folder_path='../trained_models')
+# search_metadata = data_loader.load_data(recommender_class.RECOMMENDER_NAME + "_metadata.zip")
+# search_metadata.keys()
+#
+# hyperparameters_df = search_metadata["hyperparameters_df"]
+# print(hyperparameters_df)
+#
+# result_on_validation_df = search_metadata["result_on_validation_df"]
+# print(result_on_validation_df)
+#
+# result_best_on_test = search_metadata["result_on_last"]
+# print(result_best_on_test)
+#
+# best_hyperparameters = search_metadata["hyperparameters_best"]
+# print(best_hyperparameters)
+#
+# print("FINAL MAP: {}".format(result_best_on_test.loc[10]["MAP"]))
